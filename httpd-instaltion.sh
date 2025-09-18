f05906536e3c3985262fb4d008ad5efe5bf90ecf
anuj sankdecha <anujsankdecha789@gmail.com>
2025-09-18 10:38:50 +0530

    Create httpd-instaltion.sh

---
+++ httpd-instaltion.sh
@@ -0,0 +1,19 @@
+yum install httpd -y
+systemctl start httpd 
+systemctl enable httpd
+systemctl status httpd
+
+firewall-cmd --permanent --add-service=http
+firewall-cmd --reload
+
+semanage port -a -t http_port_t -p tcp 85
+semanage port -l | grep http
+
+firewall-cmd --permanent --add-port=85/tcp
+firewall-cmd --reload
+
+echo "<h1>hello anuj</h1>" > /var/www/html/index.html
+
+systemctl restart httpd
+
+echo "httpd installation complete on port 85"
