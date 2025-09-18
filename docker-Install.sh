cb17aec8018833e05fb8da1cf59fcdcd49ff4aa6
anuj sankdecha <anujsankdecha789@gmail.com>
2025-09-18 10:51:17 +0530

    add

---
+++ docker-Install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cd /etc/yum.repos.d/
+
+# Downloading the Docker repository file
+wget -O docker.repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce --nobest -y
+cd~
+systemctl start docker
+systemctl enable docker
+systemctl status docker
+
+# Pulling the latest httpd image from Docker Hub
+docker run -dit --name webserver -p 80:80 httpd:latest  
+docker ps -a
+
+# Checking the installed Docker version
+docker --version    
+
+echo "docker installation complete"
--- httpd.sh
+++ httpd.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 yum install httpd -y
 systemctl start httpd 
 systemctl enable httpd
@@ -6,12 +7,15 @@ systemctl status httpd
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
 
+# Modifying SELinux to allow httpd to run on a non-standard port (85)
 semanage port -a -t http_port_t -p tcp 85
 semanage port -l | grep http
 
+# Configuring the firewall to allow traffic on port 85
 firewall-cmd --permanent --add-port=85/tcp
 firewall-cmd --reload
 
+# Creating a custom index.html file
 echo "<h1>hello anuj</h1>" > /var/www/html/index.html
 
 systemctl restart httpd
