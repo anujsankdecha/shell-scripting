bba410ecc716403a48dab5c68813a618fac5e914
anuj sankdecha <anujsankdecha789@gmail.com>
2025-10-16 10:07:49 +0530

    Update prometheus.sh

--- prometheus.sh
+++ prometheus.sh
@@ -24,10 +24,31 @@ cp prometheus /usr/local/bin
 cp promtool /usr/local/bin/
 chown -R prometheus:prometheus /usr/local/bin/prometheus 
 chown -R prometheus:prometheus /usr/local/bin/promtool 
-    
+
 # Create a systemd service file for Prometheus
 
-vim /etc/systemd/system/prometheus.service
+sudo tee /etc/systemd/system/prometheus.service > /dev/null <<EOF
+[Unit]
+Description=Prometheus
+Wants=network-online.target
+After=network-online.target
+
+[Service]
+User=prometheus
+Group=prometheus
+Type=simple
+ExecStart=/usr/local/bin/prometheus \
+    --config.file /etc/prometheus/prometheus.yml \
+    --storage.tsdb.path /var/lib/prometheus/ \
+    --web.console.templates=/etc/prometheus/consoles \
+    --web.console.libraries=/etc/prometheus/console_libraries
+
+[Install]
+WantedBy=multi-user.target
+EOF  
+  
+# Create a systemd service file for Prometheus
+
 systemctl daemon-reload 
 systemctl start promotheus.service
 systemctl start prometheus.service
